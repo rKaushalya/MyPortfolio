4f8e5ecac2999c907500576e75f6e8855424d22d
ravinduKaushaly <ravinduk56@gmail.com>
2023-05-22 23:53:37 +0530

    Assignment Directory Change

---
+++ assest/js.js
@@ -0,0 +1,19 @@
+
+const text = document.getElementById("typingText");
+
+const textLoad = ()=> {
+    setTimeout(() => {
+        text.textContent = "Web Designing";
+    },0);
+    setTimeout(() => {
+        text.textContent = "Web Development";
+    },4000);
+    setTimeout(() => {
+        text.textContent = "Android Development";
+    },8000);
+    setTimeout(() => {
+        text.textContent = "Fullstack Development";
+    },12000);
+}
+textLoad();
+setInterval(textLoad, 16000);
