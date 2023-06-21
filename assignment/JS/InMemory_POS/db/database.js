7e4ad3dd216d73091a770396bd5e34ab7baff672
rKaushalya <ravinduk56@gmail.com>
2023-06-21 18:31:15 +0530

    Implement DB and Model Package

---
+++ assignment/JS/InMemory_POS/db/database.js
@@ -0,0 +1,12 @@
+
+var customerDB = [
+    {id:"C00-001",name:"Ravindu Kaushalya",address: "Galle",contact: "0765320187",salary: 300000},
+    {id:"C00-002",name:"Saman Kumara",address: "Matara",contact: "0765320187",salary: 100000}
+];
+
+var itemDB = [
+    {code:"I00-001",name:"detol",price:200,qty:200},
+    {code:"I00-002",name:"kakulu hal",price:100,qty:250}
+];
+
+var orderDB = [];
